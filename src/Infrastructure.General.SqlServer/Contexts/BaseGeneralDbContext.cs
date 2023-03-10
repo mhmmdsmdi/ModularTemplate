dd9d2c4cc2cfd115299c49877edb20a7cd1f5afd
Mohammad Samadi <mhmmdsmdi@gmail.com>
2023-03-10 19:37:28 +0330

    feat: create base projects

---
+++ src/Infrastructure.General.SqlServer/Contexts/BaseGeneralDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.General.SqlServer.Contexts;
+
+public abstract class BaseGeneralDbContext : DbContext
+{
+    protected abstract string Schema { get; }
+
+    protected BaseGeneralDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+}
